b2e74c4da57d03cd5750873a840bb8c7dcc4f7f4
michaldra <mich.dra10@gmail.com>
2024-12-10 19:36:10 +0100

    10.12

--- bot_discord.py
+++ bot_discord.py
@@ -30,32 +30,32 @@ async def bye(ctx):
 async def meme(ctx):
     rng = random.randint(0,99)
     if rng < 50:
-        img_name = random.choice(os.listdir('images/common'))
-        with open(f'images/common/{img_name}', 'rb') as f:
+        img_name = random.choice(os.listdir('memes/1common'))
+        with open(f'memes/1common/{img_name}', 'rb') as f:
                 picture = discord.File(f)
         await ctx.send(file=picture)
         await ctx.send('Popularny (50%)')
     elif rng < 75:
-        img_name = random.choice(os.listdir('images/uncommon'))
-        with open(f'images/uncommon/{img_name}', 'rb') as f:
+        img_name = random.choice(os.listdir('memes/2uncommon'))
+        with open(f'memes/2uncommon/{img_name}', 'rb') as f:
                 picture = discord.File(f)
         await ctx.send(file=picture)
         await ctx.send('Niepopularny (25%)')
     elif rng < 90:
-        img_name = random.choice(os.listdir('images/rare'))
-        with open(f'images/rare/{img_name}', 'rb') as f:
+        img_name = random.choice(os.listdir('memes/3rare'))
+        with open(f'memes/3rare/{img_name}', 'rb') as f:
                 picture = discord.File(f)
         await ctx.send(file=picture)
         await ctx.send('Rzadki (15%)')
     elif rng < 99:
-        img_name = random.choice(os.listdir('images/epic'))
-        with open(f'images/epic/{img_name}', 'rb') as f:
+        img_name = random.choice(os.listdir('memes/4epic'))
+        with open(f'memes/4epic/{img_name}', 'rb') as f:
                 picture = discord.File(f)
         await ctx.send(file=picture)
         await ctx.send('Epicki (9%)')
     else:
-        img_name = random.choice(os.listdir('images/legendary'))
-        with open(f'images/legendary/{img_name}', 'rb') as f:
+        img_name = random.choice(os.listdir('memes/5legendary'))
+        with open(f'memes/5legendary/{img_name}', 'rb') as f:
                 picture = discord.File(f)
         await ctx.send(file=picture)
         await ctx.send('Legendarny (1%)')
@@ -78,4 +78,17 @@ async def ai(ctx):
             await(attachment.save(f"ai_imgs/{attachment.filename}"))
             await ctx.send(bot_logic.detect_trash(f"ai_imgs/{attachment.filename}", "keras_model.h5", "labels.txt"))
 
-bot.run("gucia")
+@bot.command()
+async def kalkulator_info(ctx):
+    await ctx.send("Oblicz efektywność energetyczną twojego domu!")
+    await ctx.send("Użyj następującej komendy: **$kalkulator X Y Z**")
+    await ctx.send("Podaj wielkość swojego domu:\n(A) - mały\n(B) - średni\n(C) - duży")
+
+@bot.command()
+async def joe(ctx):
+    await ctx.send(f'joe MAMA')
+    with open(f'memes\laughter-cartoon.png', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+bot.run("hansko")
